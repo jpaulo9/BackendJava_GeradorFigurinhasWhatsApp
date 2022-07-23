da031cae296870d5e46efb5252272994e0078df1
jpaulo9 <jpaulosilva598@gmail.com>
2022-07-23 20:27:47 -0300

    One Commit

---
+++ src/test/java/dev9/java/springjava/SpringjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev9.java.springjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
