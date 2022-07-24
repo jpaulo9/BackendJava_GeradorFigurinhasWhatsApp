776ae8061900912996a345e9e5c09c8b042c5772
jpaulo9 <67615049+jpaulo9@users.noreply.github.com>
2022-07-23 21:40:40 -0300

    Delete src directory

--- src/main/java/dev9/java/springjava/LinguagemRepository.java
+++
@@ -1,8 +0,0 @@
-package dev9.java.springjava;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LinguagemRepository extends MongoRepository<Linguagem, String> {
-
-
-}
