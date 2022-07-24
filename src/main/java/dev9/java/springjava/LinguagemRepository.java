6e6bf502faf94b251a7c869cb1a09a25f476482e
jpaulo9 <jpaulosilva598@gmail.com>
2022-07-23 21:58:09 -0300

    Commit One

---
+++ src/main/java/dev9/java/springjava/LinguagemRepository.java
@@ -0,0 +1,8 @@
+package dev9.java.springjava;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LinguagemRepository extends MongoRepository<Linguagem, String> {
+
+
+}
